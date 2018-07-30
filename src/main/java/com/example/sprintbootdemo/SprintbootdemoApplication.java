a9982121580bd8ffbf9a06e7498a6cb518a14933
xgy221 <17302558812@163.com>
2018-07-30 14:38:40 +0800

    spring boot

---
+++ src/main/java/com/example/sprintbootdemo/SprintbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootdemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SprintbootdemoApplication.class, args);
+	}
+}
